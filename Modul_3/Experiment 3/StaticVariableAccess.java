dc778d214973e9f2f137ef42d7f5f714fe9bc5ff
Ganno Tribuana Kurniaji <44857363+L200184092@users.noreply.github.com>
2019-09-20 16:51:40 +0700

    Delete StaticVariableAccess.java

--- Modul_3/Experiment 3/StaticVariableAccess.java
+++
@@ -1,8 +0,0 @@
-public class StaticVariableAccess {
-    public static void main(String[] args) {
-        StaticVariable.akreditasi = 'B';
-        StaticVariable sv = new StaticVariable();
-        sv.firstMethod();
-        sv.secondMethod();
-    }
-}
